742c5a0b210b15eebc24d7835c96899f8673d079
ABDOULAYE BARRY <lawt71792@gmail.com>
2025-05-04 18:07:51 +0200

    mise à jour sur l'authentification de l'utilisateur

--- ecommerce/src/main/java/fr/orleans/m1/wsi/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/fr/orleans/m1/wsi/ecommerce/EcommerceApplication.java
@@ -2,8 +2,12 @@ package fr.orleans.m1.wsi.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import fr.orleans.m1.wsi.ecommerce.configSecurity.RsaKeyProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
