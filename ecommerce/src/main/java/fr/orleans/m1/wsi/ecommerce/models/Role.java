b108c1902acc137926ea124fb3041b97d1b71e8e
ABDOULAYE BARRY <abdoulaye.barry@etu.univ-orleans.fr>
2025-06-27 03:18:40 +0200

    retouche sur l'entité user et ses roles

--- ecommerce/src/main/java/fr/orleans/m1/wsi/ecommerce/models/Role.java
+++ ecommerce/src/main/java/fr/orleans/m1/wsi/ecommerce/models/Role.java
@@ -1,22 +1,28 @@
 package fr.orleans.m1.wsi.ecommerce.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "roles")
 public class Role {
 
+    @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    
-    public Role() {}
-    
-    public Role(String name) {
-        this.name = name;
-    }
+
+    @Enumerated(EnumType.STRING)
+    private ERoles eRoles;
 }
--- ecommerce/src/main/java/fr/orleans/m1/wsi/ecommerce/models/Users.java
+++ ecommerce/src/main/java/fr/orleans/m1/wsi/ecommerce/models/Users.java
@@ -1,21 +1,22 @@
 package fr.orleans.m1.wsi.ecommerce.models;
 
 
+import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Builder
 @AllArgsConstructor
 @Getter
 @Setter
@@ -23,6 +24,7 @@ import lombok.Setter;
 public class Users {
     // Fields
 
+    @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
     
@@ -35,13 +37,6 @@ public class Users {
     @Column(nullable = false)
     private String password;
 
-    @ManyToMany(fetch = jakarta.persistence.FetchType.EAGER)
-    @JoinTable(
-    name = "users_roles", 
-    joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), 
-    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
-    private Set<Role> roles;
-
     @Column(nullable = false)
     private Long count;
 
@@ -51,14 +46,10 @@ public class Users {
     @Column(nullable = false)
     private String phoneNumber;
 
-    // Constructors
-    public Users() {}
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable( name = "users_roles", 
+        joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), 
+        inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles = new HashSet<>();
 
-    public Users(String email, String username, String password, String address, String phoneNumber) {
-        this.email = email;
-        this.username = username;
-        this.password = password;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-    }
 }
