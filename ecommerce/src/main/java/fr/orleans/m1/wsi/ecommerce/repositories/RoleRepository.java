8a8dc017b8dc68337a606440a0f6afd1a6029d50
ABDOULAYE BARRY <lawt71792@gmail.com>
2025-07-05 09:52:27 +0200

    mon role repository

---
+++ ecommerce/src/main/java/fr/orleans/m1/wsi/ecommerce/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package fr.orleans.m1.wsi.ecommerce.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fr.orleans.m1.wsi.ecommerce.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByERole(String role);
+}
